653d1bd867fb146c5e63d930f122d9e46c496bd2
aaron-soto <aaron.m.soto1@gmail.com>
2024-11-13 17:02:51 -0700

    fix: adjust background opacity for improved visibility on marketing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -6,7 +6,7 @@ import { SocialButtons } from "@/app/(marketing)/social-buttons";
 export default function Home() {
   return (
     <div className="w-screen h-dvh relative">
-      <div className="absolute inset-0 bg-black bg-opacity-80 z-[1000]"></div>
+      <div className="absolute inset-0 bg-black bg-opacity-50 md:bg-opacity-80 z-[1000]"></div>
       <Image
         src="/images/william-hero.jpg"
         alt="hero"
