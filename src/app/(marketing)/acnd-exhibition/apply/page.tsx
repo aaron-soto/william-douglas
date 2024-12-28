5778c1a2d0f30145ffa8c2458f42d8c8b31ce242
aaron-soto <aaron.m.soto1@gmail.com>
2024-12-28 09:20:36 -0700

    feat: enhance ACND Exhibition application page layout and iframe styling

--- src/app/(marketing)/acnd-exhibition/apply/page.tsx
+++ src/app/(marketing)/acnd-exhibition/apply/page.tsx
@@ -1,10 +1,18 @@
 export default function Page() {
   return (
     <>
-      <div className="py-6 px-4 text-white flex flex-col">
+      <div className="py-6 px-4 text-white flex flex-col h-screen">
         <h1 className="text-3xl text-white">ACND Exhibition Booth Application</h1>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeWspL34bhZp6fYInCw4u_IDlVyoUy1_BobGVe-tUDegmztDQ/viewform?embedded=true" width="640" height="960" frameBorder="0" marginHeight={0} marginWidth={0} className=" mt-6">Loading…</iframe>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSeWspL34bhZp6fYInCw4u_IDlVyoUy1_BobGVe-tUDegmztDQ/viewform?embedded=true"
+          frameBorder="0"
+          marginHeight={0}
+          marginWidth={0}
+          className="mt-6 h-full w-full"
+        >
+          Loading…
+        </iframe>
       </div>
     </>
-  )
+  );
 }
