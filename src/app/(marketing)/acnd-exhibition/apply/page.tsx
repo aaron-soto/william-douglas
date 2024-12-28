87b345339f1a5d38bd7ffb981dcadb3c3337ff54
aaron-soto <aaron.m.soto1@gmail.com>
2024-12-28 09:13:03 -0700

    fix: update marginHeight and marginWidth attributes to use curly braces for proper rendering in ACND Exhibition application page

--- src/app/(marketing)/acnd-exhibition/apply/page.tsx
+++ src/app/(marketing)/acnd-exhibition/apply/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
     <>
       <div className="py-6 px-4 text-white flex flex-col">
         <h1 className="text-3xl text-white">ACND Exhibition Booth Application</h1>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeWspL34bhZp6fYInCw4u_IDlVyoUy1_BobGVe-tUDegmztDQ/viewform?embedded=true" width="640" height="960" frameBorder="0" marginHeight="0" marginWidth="0" className=" mt-6">Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeWspL34bhZp6fYInCw4u_IDlVyoUy1_BobGVe-tUDegmztDQ/viewform?embedded=true" width="640" height="960" frameBorder="0" marginHeight={0} marginWidth={0} className=" mt-6">Loading…</iframe>
       </div>
     </>
   )
