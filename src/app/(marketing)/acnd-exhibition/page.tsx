18119c0b0e556679e0ae659c6255a7d0d14f7bae
aaron-soto <aaron.m.soto1@gmail.com>
2025-04-02 15:19:47 -0700

    feat: add waitlist page for ACND Exhibition Fall 2025 and update exhibition details

--- src/app/(marketing)/acnd-exhibition/page.tsx
+++ src/app/(marketing)/acnd-exhibition/page.tsx
@@ -29,34 +29,79 @@ export default function Page() {
     <div className="py-6 px-4 text-white flex flex-col">
       <h1 className="text-3xl text-white">ACND Exhibition</h1>
 
-      <Image src="/images/acnd/acnd-color-logo.png" height={50} width={200} alt="acnd-logo" className="my-4" />
+      <Image
+        src="/images/acnd/acnd-color-logo.png"
+        height={50}
+        width={200}
+        alt="acnd-logo"
+        className="my-4"
+      />
 
       <p className="text-muted-foreground mt-3">
-        The ACND Art Exhibition was created to bridge the gap between serious artists, makers, and craftspeople and their ideal clients and customers. What sets this exhibition apart is its focus on connecting creators with genuine buyers rather than other creators, ensuring a more meaningful and lucrative experience for participants.
+        The ACND Art Exhibition was created to bridge the gap between serious
+        artists, makers, and craftspeople and their ideal clients and customers.
+        What sets this exhibition apart is its focus on connecting creators with
+        genuine buyers rather than other creators, ensuring a more meaningful
+        and lucrative experience for participants.
       </p>
       <p className="text-muted-foreground mt-3">
-        The concept for ACND began with William Douglas’s own journey as a maker. Early on, he participated in events to gain exposure, but his high-end, art exhibition furniture often stood out in the wrong way. Many attendees were there for affordable holiday trinkets, not investment pieces. Realizing he needed a different platform but unable to afford the $15,000 cost to join the &quot;right&quot; shows in LA, William decided to create his own opportunity.
+        The concept for ACND began with William Douglas’s own journey as a
+        maker. Early on, he participated in events to gain exposure, but his
+        high-end, art exhibition furniture often stood out in the wrong way.
+        Many attendees were there for affordable holiday trinkets, not
+        investment pieces. Realizing he needed a different platform but unable
+        to afford the $15,000 cost to join the &quot;right&quot; shows in LA,
+        William decided to create his own opportunity.
       </p>
       <p className="text-muted-foreground mt-3">
-        Hosted at Case Study Coffee Lounge, ACND is designed to provide serious artists and makers with the kind of support William didn’t have early in his career—a professional space that connects them with buyers who value and appreciate their work.
+        Hosted at Case Study Coffee Lounge, ACND is designed to provide serious
+        artists and makers with the kind of support William didn’t have early in
+        his career—a professional space that connects them with buyers who value
+        and appreciate their work.
       </p>
       <p className="text-muted-foreground mt-3">
-        This event is not for hobbyists, as there are already many avenues for them to showcase their creations. Instead, ACND is dedicated to supporting emerging professionals who are serious about their craft and ready to grow their business.
+        This event is not for hobbyists, as there are already many avenues for
+        them to showcase their creations. Instead, ACND is dedicated to
+        supporting emerging professionals who are serious about their craft and
+        ready to grow their business.
       </p>
 
       <div className="flex flex-col my-4  gap-3 mb-6">
-        <Link className="hover:text-red-700 underline text-white" href="/acnd-exhibition/apply">ACND SPRING 2025 Application</Link>
-        <Link className="hover:text-red-700 underline text-white" href="https://www.instagram.com/acnd.exhibition/" target="_blank">Follow ACND on Instagram</Link>
+        <div className="flex items-center gap-2">
+          <Link
+            className="text-neutral-500 line-through pointer-events-none"
+            href="/acnd-exhibition/apply"
+          >
+            ACND SPRING 2025 Application
+          </Link>
+          <span className="text-sm -translate-y-2 -rotate-3 -translate-x-4">
+            Sold Out
+          </span>
+        </div>
+        <Link
+          className="hover:text-red-700 underline text-white"
+          href="/acnd-exhibition/waitlist"
+        >
+          Join Waitlist for Fall 2025 ACND
+        </Link>
+        <Link
+          className="hover:text-red-700 underline text-white"
+          href="https://www.instagram.com/acnd.exhibition/"
+          target="_blank"
+        >
+          Follow ACND on Instagram
+        </Link>
       </div>
 
       <h2 className=" text-xl font-sans uppercase">ACND 2024</h2>
 
       <ImageSlider images={IMAGES} className="w-full mt-4" />
 
-      <h2 className="text-xl font-sans uppercase mt-12">What people say about ACND</h2>
+      <h2 className="text-xl font-sans uppercase mt-12">
+        What people say about ACND
+      </h2>
 
       <ACNDTestimonialSlider className="w-full" />
-
     </div>
   );
 }
---
+++ src/app/(marketing)/acnd-exhibition/waitlist/page.tsx
@@ -0,0 +1,19 @@
+export default function Page() {
+  return (
+    <>
+      <div className="py-6 px-4 text-white flex flex-col h-screen">
+        <h1 className="text-3xl text-white">
+          ACND Exhibition Fall 2025 Waitlist
+        </h1>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdwLrrsGIubslUoMRqExxUSwGWAy3MsFpAAz3OpJtkRjqHwOQ/viewform?embedded=true"
+          // width="640"
+          // height="824"
+          className="mt-6 h-full w-full"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </>
+  );
+}
